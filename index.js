22f6e48792f84a15a64b9f06c44313b2d38714ee
Daniil Praktika <78875859+mqpanda@users.noreply.github.com>
2024-02-29 16:28:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 1501;
+const PORT = process.env.PORT || 1502;
 
 // Разрешить все источники (*), вы можете настроить это для конкретных доменов
 app.use(cors());
